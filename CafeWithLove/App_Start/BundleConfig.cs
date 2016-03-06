6e31bdf8f3603954b06b85c28f49fc96561b973c
teckloon <loondo@gmail.com>
2016-03-06 17:51:05 +0800

    Testing if it is casesensitive

--- CafeWithLove/App_Start/BundleConfig.cs
+++ CafeWithLove/App_Start/BundleConfig.cs
@@ -28,7 +28,7 @@ namespace CafeWithLove
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
+                      "~/Content/Site.css",
                       "~/Content/bootflat.css"));
         }
     }
