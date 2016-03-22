6279b2246c0e231cd539c283cdcef5dac9825667
meixi-l <lim.m3ixi@gmail.com>
2016-03-22 16:08:58 +0800

    bookmarks page + update database + optimise cafemapper

--- CafeWithLove/DAL/CafeMapper.cs
+++ CafeWithLove/DAL/CafeMapper.cs
@@ -11,37 +11,24 @@ namespace CafeWithLove.DAL
     {
         private CafeDetailGateway cafeDetailGateway = new CafeDetailGateway();
         private CafeOutletGateway cafeOutletGateway = new CafeOutletGateway();
-        //public List<CafeViewModel> temp;
+
         List<CafeViewModel> modelList = new List<CafeViewModel>();
 
         public ICollection<CafeViewModel> CafeMap(String searchInput)
         {
-            IEnumerable<CafeDetail> CafeList = cafeDetailGateway.Search(searchInput);
-            
-            foreach (CafeDetail cafe in CafeList)
-            {
-                ICollection<CafeOutlet> outletList = cafeOutletGateway.getOutlet(cafe.Id);
-                CafeViewModel tempmodel = new CafeViewModel();
-                tempmodel.CafeDetailVM = cafe;
-                tempmodel.CafeOutletVM = outletList;
-                modelList.Add(tempmodel);
-            }
+            IEnumerable<CafeDetail> cafeList = cafeDetailGateway.Search(searchInput);
+
+            MapOutletToCafe(cafeList);
+
             return modelList;
         }
 
         public ICollection<CafeViewModel> CafeMapAll()
         {
-            IEnumerable<CafeDetail> CafeList = cafeDetailGateway.SelectAll();
+            IEnumerable<CafeDetail> cafeList = cafeDetailGateway.SelectAll();
 
-            foreach (CafeDetail cafe in CafeList)
-            {
-                ICollection<CafeOutlet> outletList = cafeOutletGateway.getOutlet(cafe.Id);
-                CafeViewModel tempmodel = new CafeViewModel();
-                tempmodel.CafeDetailVM = cafe;
-                tempmodel.CafeOutletVM = outletList;
-                modelList.Add(tempmodel);
-            }
-            
+            MapOutletToCafe(cafeList);
+
             return modelList;
         }
 
@@ -57,6 +44,8 @@ namespace CafeWithLove.DAL
             return tempmodel;
         }
 
+        // different from the rest
+        // gets CafeOutlet before CafeDetails
         public ICollection<CafeViewModel> CafeMapBookmarks(int[] cafeOutletIds)
         {
             ICollection<CafeOutlet> outletList = cafeOutletGateway.getBookmarked(cafeOutletIds);        // get list of outlets
@@ -79,21 +68,13 @@ namespace CafeWithLove.DAL
 
         public ICollection<CafeViewModel> MostVisited()
         {
-            IEnumerable<CafeDetail> CafeList = cafeDetailGateway.MostVisited();
-
-            foreach (CafeDetail cafe in CafeList)
-            {
-                ICollection<CafeOutlet> outletList = cafeOutletGateway.getOutlet(cafe.Id);
-                CafeViewModel tempmodel = new CafeViewModel();
-                tempmodel.CafeDetailVM = cafe;
-                tempmodel.CafeOutletVM = outletList;
-                modelList.Add(tempmodel);
-            }
+            IEnumerable<CafeDetail> cafeList = cafeDetailGateway.MostVisited();
+            
+            MapOutletToCafe(cafeList);
 
             return modelList;
         }
-
-
+        
         public ICollection<CafeViewModel> CafePFilter(string chosen)
         {
             IEnumerable<CafeDetail> CafeList = cafeDetailGateway.PFilter(chosen);
@@ -109,5 +90,23 @@ namespace CafeWithLove.DAL
             return modelList;
         }
 
+        // query CafeOutlet database based on list of cafes from CafeDetails
+        // map each CafeOutlet to CafDetails based on cafeId
+        public void MapOutletToCafe(IEnumerable<CafeDetail> cafeList)
+        {
+            int[] cafeIds = cafeList.Select(cafe => cafe.Id).Distinct().ToArray();
+
+            ICollection<CafeOutlet> outletList = cafeOutletGateway.getBookmarked(cafeIds);        // get list of outlets
+
+            foreach (CafeDetail cafe in cafeList)      // loop through all cafes in cafeList
+            {
+                ICollection<CafeOutlet> outlets = new List<CafeOutlet>();
+                outlets = outletList.Where(outlet => outlet.cafeId == cafe.Id).ToList();
+                CafeViewModel tempmodel = new CafeViewModel();
+                tempmodel.CafeDetailVM = cafe;
+                tempmodel.CafeOutletVM = outlets;       // store all outlets into the same cafedetail
+                modelList.Add(tempmodel);
+            }
+        }
     }
 }
